24de5dcc1fd9e4d20558c15b07e03784e30a1fb6
kangshengsheng <15201224529@139.com>
2017-03-28 01:02:47 +0800

    添加第三个页面

--- app/src/main/java/com/ty/app/yxapp/dwcenter/ui/Activity/fragment/MainThirdPagerActivity.java
+++ app/src/main/java/com/ty/app/yxapp/dwcenter/ui/Activity/fragment/MainThirdPagerActivity.java
@@ -2,8 +2,20 @@ package com.ty.app.yxapp.dwcenter.ui.Activity.fragment;
 
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ty.app.yxapp.dwcenter.R;
+import com.ty.app.yxapp.dwcenter.Utils.AndroidUtils;
 import com.ty.app.yxapp.dwcenter.ui.Activity.base.BaseFragment;
+import com.ty.app.yxapp.dwcenter.ui.Widget.ImageButtonCell;
+import com.ty.app.yxapp.dwcenter.ui.Widget.LooperImgCell;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kss on 2017/3/26.
@@ -11,17 +23,46 @@ import com.ty.app.yxapp.dwcenter.ui.Activity.base.BaseFragment;
 
 public class MainThirdPagerActivity extends BaseFragment {
     private Context context;
-
-    @Override
-    public View onCreate() {
-        context = getContext();
-        TextView textView = new TextView(context);
-        textView.setText("MainThirdPagerActivity");
-        return textView;
-    }
+    private LooperImgCell looperImgCell;
 
     @Override
     public void onBeforeCreate() {
 
     }
+
+    @Override
+    public View onCreate() {
+        context = getContext();
+        ScrollView scrollView = new ScrollView(context);
+
+        LinearLayout cont = new LinearLayout(context);
+        cont.setOrientation(LinearLayout.VERTICAL);
+        scrollView.addView(cont,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        actionBar.setVisibility(View.VISIBLE);
+        actionBar.setCenterView(AndroidUtils.getString(R.string.user_center));
+        actionBar.setRightView("", R.mipmap.ic_launcher, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"饿呢奥尔够昂偶", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        looperImgCell = new LooperImgCell(context);
+        cont.addView(looperImgCell,new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                AndroidUtils.dp(200)));
+
+        init();
+        return scrollView;
+    }
+
+    private void init(){
+        List<Integer> resList = new ArrayList<>();
+        for(int i=0;i<3;i++){
+            resList.add(R.drawable.timg);
+        }
+        looperImgCell.setResList(resList);
+    }
+
 }
